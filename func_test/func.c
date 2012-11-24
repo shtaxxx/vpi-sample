e3227c3eb91849360d1f56171d7bd357103e7833
shtaxxx <shta.ky1018@gmail.com>
2012-11-24 23:41:40 +0900

    every tab indent are replaced with two-spaces

--- func_test/func.c
+++ func_test/func.c
@@ -7,22 +7,22 @@ static int func_compiletf(char*user_data)
 
 static int func_calltf(char*user_data)
 {
-  vpiHandle	vhandle, iterator, arg;
-	s_vpi_value	inval, outval;
-	int	din, dout;
-	vhandle = vpi_handle(vpiSysTfCall, NULL);
-	iterator = vpi_iterate(vpiArgument, vhandle);
-	arg = vpi_scan(iterator);
+  vpiHandle vhandle, iterator, arg;
+  s_vpi_value inval, outval;
+  int din, dout;
+  vhandle = vpi_handle(vpiSysTfCall, NULL);
+  iterator = vpi_iterate(vpiArgument, vhandle);
+  arg = vpi_scan(iterator);
 
-	inval.format = vpiIntVal;
-	vpi_get_value(arg, &inval);
-	din = inval.value.integer;
+  inval.format = vpiIntVal;
+  vpi_get_value(arg, &inval);
+  din = inval.value.integer;
 
   vpi_printf("din=%d\n", din);
   dout = din * 2;
 
-	outval.format = vpiIntVal;
-	outval.value.integer = dout;
+  outval.format = vpiIntVal;
+  outval.value.integer = dout;
   vpi_put_value(vhandle, &outval, NULL, vpiNoDelay);
 
   return 0;
@@ -35,18 +35,18 @@ static int func_retsize()
 
 void func_register()
 {
-      s_vpi_systf_data tf_data;
-      tf_data.type        = vpiSysFunc;
-      tf_data.sysfunctype = vpiIntFunc;
-      tf_data.tfname    = "$func_test";
-      tf_data.calltf    = func_calltf;
-      tf_data.compiletf = func_compiletf;
-      tf_data.sizetf    = func_retsize;
-      tf_data.user_data = 0;
-      vpi_register_systf(&tf_data);
+  s_vpi_systf_data tf_data;
+  tf_data.type        = vpiSysFunc;
+  tf_data.sysfunctype = vpiIntFunc;
+  tf_data.tfname    = "$func_test";
+  tf_data.calltf    = func_calltf;
+  tf_data.compiletf = func_compiletf;
+  tf_data.sizetf    = func_retsize;
+  tf_data.user_data = 0;
+  vpi_register_systf(&tf_data);
 }
 
 void (*vlog_startup_routines[])() = {
-    func_register,
-    0
+  func_register,
+  0
 };
